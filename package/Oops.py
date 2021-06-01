7629b0e61a361b12c73674fb2166f6a45fd6a40a
AmbikaPuvvula <45551971+AmbikaPuvvula@users.noreply.github.com>
2021-06-01 18:41:53 +0530

    Packages and Modules using OOPS

---
+++ package/Oops.py
@@ -0,0 +1,18 @@
+class Mymath():
+    '''About packages and modules using classes'''
+    def __init__(self,num1,num2):
+        self.val1 = num1
+        self.val2 = num2
+    def add(self):
+        return self.val1 +self.val2
+    
+class demo:
+    a, b = 12,18
+    c,d = 14,16
+    def add():
+        a = 6
+        b = 10
+        print(a+b)
+    def sub():
+        c,d = 14,16
+        print(d-c)
---
+++ package/Oops2.py
@@ -0,0 +1,7 @@
+class Modu:
+    def power(bval,eval):
+        return bval**eval
+    def isEven(val):
+        if val % 2 == 0:
+            return True
+        return False
